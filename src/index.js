4d6726d14d6c50a6de6215acdfc6abab1d20ac71
Mohit <mohit.peak@gmail.com>
2024-05-13 18:35:52 +0530

    Added response time middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const responseTime = require("response-time");
 
 const app = express();
 
 const APIrouter = require("./routes/api_router");
 
 const { PORT } = require("./config/serverConfig");
+
+app.use(responseTime());
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", APIrouter);
 
